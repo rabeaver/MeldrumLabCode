173cb5f89bda0440b90ff00d4b245b16ca9cf2ae
Tyler Meldrum <tkmeldrum@wm.edu>
2015-07-10 16:55:30 -0400

    PM25 Desktop End of Day push

--- MatlabProc_23June2015_CHIRP.m
+++ MatlabProc_23June2015_CHIRP.m
@@ -6,7 +6,7 @@ close all
 %%
 % CHIRP params
 
-Pchirp = 0.010; % CHIRP Pulse Length (s)
+Pchirp = 0.015; % CHIRP Pulse Length (s)
 BWchirp = 11223; % CHIRP bandwidth (Hz)
 
 nPts = 67; % # of acqu points
@@ -31,8 +31,8 @@ z = f/280.47;           %um, 280.47 Hz/um (for PM25)
 
 %%
 
-datadir = '/Users/jaredking/Documents/Chemistry/Research_Files_and_Data/CHIRP/Good_Data_Sets/';
-datafile = 'CHIRP2D_15mM_GdH2O_10mspulse_ampon_40um_6July2015';
+datadir = 'C:\Users\NMRLab\Desktop\CHIRP\';
+datafile = 'CHIRP_GdH2O_50mM_15mspulse_10July2015';
 
 % Import CHIRP data
 [~ , spec, spec2, ~] = readTecmag4d(strcat(datadir,datafile,'.tnt'));
@@ -86,7 +86,7 @@ hold off
 
 %% No CHIRP load section
 
-filenameNO = 'CHIRP2D_15mM_GdH2O_10mspulse_ampoff_40um_6July2015';
+filenameNO = 'noCHIRP_GdH2O_50mM_15mspulse_10July2015';
 [~,spec,spec2] = readTecmag4d(strcat(datadir,filenameNO,'.tnt'));
 data = reshape(spec,nPts,nEchoes);
 
@@ -169,9 +169,9 @@ plot(abs(T1T2profiles(:,1)))
 
 %% Data Range and Inversion
 % manually select indices for data range and inversion (zero point)
-minind= 105;
-maxind = 142;
-firstinvertedind = 134;
+minind= 121;
+maxind = 153;
+firstinvertedind = 135;
  
 % automatically select indices
 % minind=find(f<-BWchirp/2,1,'last');
@@ -193,6 +193,8 @@ plot(t1*1000,T1T2data(:,2),'linewidth',2)
 xlabel('{\it t}_1 (ms)','fontsize',30)
 title('T1-T2, first T1 column')
 set(gca,'Fontsize',30,'linewidth',2)
+xlim([0 1000*Pchirp])
+ylim([-1.1 1.1])
 
 %% surf of all D-T2 Profiles
 
