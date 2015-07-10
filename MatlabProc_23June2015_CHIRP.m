8ecc8d3ff7aa413a912c504ecba9c3ff8e8d2e79
Tyler Meldrum <tkmeldrum@wm.edu>
2015-07-10 19:30:54 -0400

    modify T1IRT2 generator to include multiple components

--- MatlabProc_23June2015_CHIRP.m
+++ MatlabProc_23June2015_CHIRP.m
@@ -5,7 +5,7 @@ close all
 
 %%
 % CHIRP params
-Pchirp = 0.010; % CHIRP Pulse Length (s)
+Pchirp = 0.015; % CHIRP Pulse Length (s)
 BWchirp = 11223; % CHIRP bandwidth (Hz)
 
 nPts = 67; % # of acqu points
@@ -29,8 +29,8 @@ f = linspace(-Fs/2,Fs/2,NFFT);          %Hz
 z = f/280.47;           %um, 280.47 Hz/um (for PM25)
 
 %%
-datadir = '/Users/jaredking/Documents/Chemistry/Research_Files_and_Data/CHIRP/Good_Data_Sets/';
-datafile = 'CHIRP2D_15mM_GdH2O_10mspulse_ampon_40um_6July2015';
+datadir = 'C:\Users\vjlee\Desktop\';
+datafile = 'CHIRP_doubleGdH2O_50mMand5mMvials_15mspulse_10July2015';
 
 % Import CHIRP data
 [~ , spec, spec2, ~] = readTecmag4d(strcat(datadir,datafile,'.tnt'));
@@ -83,7 +83,7 @@ hold off
 
 
 %% No CHIRP load section
-filenameNO = 'CHIRP2D_15mM_GdH2O_10mspulse_ampoff_40um_6July2015';
+filenameNO = 'noCHIRP_doubleGdH2O_50mMand5mMvials_15mspulse_10July2015';
 [~,spec,spec2] = readTecmag4d(strcat(datadir,filenameNO,'.tnt'));
 data = reshape(spec,nPts,nEchoes);
 
@@ -166,9 +166,9 @@ plot(abs(T1T2profiles(:,1)))
 
 %% Data Range and Inversion
 % manually select indices for data range and inversion (zero point)
-minind= 105;
-maxind = 142;
-firstinvertedind = 134;
+minind= 99;
+maxind = 154;
+firstinvertedind = 133;
  
 % automatically select indices
 % minind=find(f<-BWchirp/2,1,'last');
@@ -213,6 +213,7 @@ echoNr = 3;
 cftool(t1,T1T2data(:,echoNr));
 %%
 
+T1T2data = T1T2data(:,2:end);
 T1T2data2 = flipud(T1T2data);
 save(strcat(datafile, '.dat'), 'T1T2data2', '-ascii')
 size(T1T2data)
