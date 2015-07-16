16b9bd4108cb3ec7c51a18d6327fb14fe3411735
Tyler Meldrum <tkmeldrum@wm.edu>
2015-07-16 16:35:06 -0400

    new CHIRP, SINC generators

--- CHIRP_generator.m
+++ CHIRP_generator.m
@@ -8,12 +8,12 @@ close all
 %%%%%%% User-defined parameters %%%%%%%
 
 % dt = 500e-9 ; %time per point in waveform (s) [Scout limit is 20ns]
-N = 20000; %points to define the waveform
-tau = 20e-3; %pulse length (s)
+N = 10000; %points to define the waveform
+tau = 10e-3; %pulse length (s)
 sliceheight = 0.040; %mm
 G = 6.59; %T m-1, B0 field gradient
 offset = 0; %mm, frequency offset (if applicable)
-amplitude = 6; %dB, for Tecmag
+amplitude = 7; %dB, for Tecmag
 % NOTE: positive offset moves to the left in the FT spectrum (negative
 % position)
 
