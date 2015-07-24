3d3e3758b1da4620aed1944ffbe1a58304249d25
Tyler Meldrum <tkmeldrum@wm.edu>
2015-07-24 11:13:29 -0400

    Updating from PM25 desktop

--- quickTecmagT2.m
+++ quickTecmagT2.m
@@ -3,14 +3,14 @@ clc
 close all
 
 %%
-filename = '1500uM_GdH2O_BeadPack_CPMG_13July2015.tnt';
-filedir = 'C:\Users\NMRLab\Desktop\CHIRP\';
+filename = 'CuH2O_1000mMstock_WHAT_CPMG_tE500u_22July2015.tnt';
+filedir = 'C:\Users\NMRLab\Desktop\CHIRP\Copper_II\';
 
 fileloc = strcat(filedir,filename);
 
 [ap,spec,spec2,spec3,spec4] = readTecmag4d(fileloc);
 tEcho = 500; %us
-nEchoes = 32;
+nEchoes = 8;
 nPts = 69;
 nPtsBlank = 5;
 
@@ -22,9 +22,9 @@ dataInt = sum(data,1);
 dataIntRe = real(dataInt)./max(real(dataInt));
 dataIntIm = imag(dataInt)./max(real(dataInt));
 
-guess = [0.3 2e-03 0.6 6e-03];
-beta = nlinfit(echoVector,dataIntRe, @t2bifit_simple, guess);
-ypred = t2bifit_simple(beta,echoVector);
+guess = [0.3 2e-03];% 0.6 0.5e-03];
+beta = nlinfit(echoVector,dataIntRe, @t2monofit_simple, guess);
+ypred = t2monofit_simple(beta,echoVector);
 
 figure(1)
 hold on
