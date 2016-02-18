4b0f35480730234c0e07df725835461df0abc116
Tyler Meldrum <tkmeldrum@wm.edu>
2016-02-18 11:05:13 -0500

    update T1Proc code for Kea

--- T1proc.m
+++ T1proc.m
@@ -8,22 +8,22 @@ clc
 % and echoes per depth
 
 % Scripts folder containing T1/T2 fitting routines
-addpath('/Users/tyler/Dropbox/Coding/Matlab Processing/Scripts/')
+% addpath('/Users/tyler/Dropbox/Coding/Matlab Processing/Scripts/')
 
-cd('/Users/tyler/Documents/Research/Aachen/Data/Pinakotheken_May2012/01May2012/WhiteTests1911/T1/c/2/')
+datadir = '/Users/tyler/Desktop/samplee/';
 parfilestem = 'acqu';
 datafilestem = 'data';
 
-params.acqTime = readpar_Kea(strcat(parfilestem,'.par'),'acqTime');
-params.bandwidth = readpar_Kea(strcat(parfilestem,'.par'),'bandwidth');
-params.nrScans = readpar_Kea(strcat(parfilestem,'.par'),'nrScans');
-params.rxPhase = readpar_Kea(strcat(parfilestem,'.par'),'rxPhase');
-params.rxGain = readpar_Kea(strcat(parfilestem,'.par'),'rxGain');
-params.nrPtsT1 = readpar_Kea(strcat(parfilestem,'.par'),'nrPntsT1');
-params.t1Max = readpar_Kea(strcat(parfilestem,'.par'),'tMax');
-params.t1Est = readpar_Kea(strcat(parfilestem,'.par'),'t1Est');
-params.repTime = readpar_Kea(strcat(parfilestem,'.par'),'repTime');
-params.b1Freq = readpar_Kea(strcat(parfilestem,'.par'),'b1Freq');
+params.acqTime = readpar_Kea(strcat(datadir,parfilestem,'.par'),'acqTime');
+params.bandwidth = readpar_Kea(strcat(datadir,parfilestem,'.par'),'bandwidth');
+params.nrScans = readpar_Kea(strcat(datadir,parfilestem,'.par'),'nrScans');
+params.rxPhase = readpar_Kea(strcat(datadir,parfilestem,'.par'),'rxPhase');
+params.rxGain = readpar_Kea(strcat(datadir,parfilestem,'.par'),'rxGain');
+params.nrPtsT1 = readpar_Kea(strcat(datadir,parfilestem,'.par'),'nrPntsT1');
+params.t1Max = readpar_Kea(strcat(datadir,parfilestem,'.par'),'tMax');
+params.t1Est = readpar_Kea(strcat(datadir,parfilestem,'.par'),'t1Est');
+params.repTime = readpar_Kea(strcat(datadir,parfilestem,'.par'),'repTime');
+params.b1Freq = readpar_Kea(strcat(datadir,parfilestem,'.par'),'b1Freq');
 
 % Make T1 time vector
 amax = 1;
@@ -36,7 +36,7 @@ for i = 1:1:params.nrPtsT1
 end
 
 % Load data
-amplitude = dlmread(strcat(datafilestem,'.dat'));
+amplitude = dlmread(strcat(datadir,datafilestem,'.dat'));
 amplitude = amplitude(:,2)./max(amplitude(:,2));
 
 %% Fit to exp decay
@@ -45,11 +45,11 @@ y0_guess = 0;
 A_guess = 1;
 t1_guess = 20; %ms
 
-guesses = [y0_guess;A_guess;t1_guess];
+guesses = [A_guess;t1_guess]; %[y0_guess;A_guess;t1_guess];
 
 CI = 90; %desired confidence interval in percent
 
-[xfit,ypred,coeffs,residuals] = T1_fit(t1TimeAxis(1:end),amplitude(1:end),guesses,CI);
+[xfit,ypred,coeffs,residuals,coeffs_err] = T1fit_no_offset(t1TimeAxis(1:end),amplitude(1:end),guesses,CI);
 
 figure
 subplot(2,1,1)
@@ -60,3 +60,7 @@ subplot(2,1,2)
 hold on
 plot(t1TimeAxis,residuals,'-b')
 plot(t1TimeAxis,zeros(length(t1TimeAxis)),'-k')
+
+
+sprintf('A = %f +/- %f',coeffs(1),coeffs_err(1))
+sprintf('T2 = %f +/- %f',coeffs(2),coeffs_err(2))
