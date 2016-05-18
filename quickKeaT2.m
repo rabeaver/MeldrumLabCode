bbf0dc0c62a1c29b27719d1e9ccc7623352bae1a
Tyler Meldrum <tkmeldrum@wm.edu>
2016-05-17 22:38:06 -0400

    Manual FT Profile Proc updates

--- quickKeaT2.m
+++ quickKeaT2.m
@@ -3,22 +3,22 @@ clc
 close all
 
 filename = 'data.1d';
-filedir = '/Volumes/ISC1026/Data/TKM/NGA_12May2016/Hamada/CDD_CPMG/1/';
+filedir = 'Z:\Data\TKM\GCI_Acrylate\17May2016\Rubber_CPMG\1\';
 fileloc = strcat(filedir,filename);
 parloc  = strcat(filedir,'acqu.par');
 
 [ap,spec] = readKea4d(fileloc);
 
 
-guess = [0.4 1]; %T2 in ms
-[beta,R,J,CovB] = nlinfit(1e-3*spec(:,1),spec(:,2), @t2monofit_simple, guess);
+guess = [0.4 1 7]; %T2 in ms
+[beta,R,J,CovB] = nlinfit(1e-3*spec(:,1),spec(:,2)./spec(1,2), @t2bifit_ampSumFixed, guess);
 ci = nlparci(beta,R,'jacobian',J);
 
-ypred = t2monofit_simple(beta,1e-3*spec(:,1));
+ypred = t2bifit_ampSumFixed(beta,1e-3*spec(:,1));
 
 hh = figure(1);
 hold on
-scatter(1e-3*spec(:,1),spec(:,2));
+scatter(1e-3*spec(:,1),spec(:,2)./spec(1,2));
 plot(1e-3*spec(:,1),ypred,'-r');
 xlabel('time/ms')
 % pubgraph(hh, 16, 2, 'w')
